fabfe2627aebccb849ac78fe35fbc04599c47d25
unknown <konancurry@gmail.com>
2023-01-30 13:42:19 +0000

    Adding workout on DOM

---
+++ Exemple2.js
@@ -0,0 +1,12 @@
+let btn = document.querySelector(".burger");
+btn.addEventListener("click", "show");
+function show(){
+    let slide = document.setAttribute(".slide")
+    return slide
+}
+ /*let bouton = document.querySelector(".cross");
+btn.addEventListener("click", "hid");
+function hid(){
+    let hide = document.setAttribute(".hidden")
+    return hide
+}*/
